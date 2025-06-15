19ec58eefb3ee10c1607ca3c369412c232d62fa3
Juanjo <juanjose.acebedolara.23@campusfp.es>
2025-06-15 21:15:57 +0200

    Adding the libraries to the files

---
+++ Stellara/src/stpch.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <memory>
+#include <algorithm>
+#include <functional>
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <Windows.h>
