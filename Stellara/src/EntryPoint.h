1dd7bc17e23ecd717f30c906359d86dc87fa15dd
Juanjo <juanjose.acebedolara.23@campusfp.es>
2025-06-17 13:13:41 +0200

    code refactoring

--- Stellara/src/EntryPoint.h
+++ Stellara/src/EntryPoint.h
@@ -5,16 +5,15 @@
 
 #ifdef ST_PLATFORM_WINDOWS
 
-extern Stellara::Application* Stellara::CreateApplication();
+	extern Stellara::Application* Stellara::CreateApplication();
 
-int main(int argc, char** argv)
-{
-	Stellara::Log::Init();
-	STLR_LOG_WARN("Initialized Log!");
+	int main(int argc, char** argv){
+		Stellara::Log::Init();
+		STLR_LOG_WARN("Initialized Log!");
 
-	auto app = Stellara::CreateApplication();
-	app->Run();
-	delete app;
-}
+		auto app = Stellara::CreateApplication();
+		app->Run();
+		delete app;
+	}
 
 #endif
