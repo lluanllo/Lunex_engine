4e092422c235f310fa86e87b5def501176a700f4
AnasEchchakeryM <anasechchakerymouh@gmail.com>
2025-06-21 15:20:53 +0200

    Implementation of WindowsInput to handle keyboard and mouse input on system

--- Stellara/src/Input.h
+++ Stellara/src/Input.h
@@ -13,6 +13,7 @@ namespace Stellara {
 			inline static std::pair<float, float> GetMousePosition() { return s_Instance->GetMousePositionImpl(); }
 			inline static float GetMouseX() { return s_Instance->GetMouseXImpl(); }
 			inline static float GetMouseY() { return s_Instance->GetMouseYImpl(); }
+			
 
 		protected:
 			virtual bool IsKeyPressedImpl(int keycode) = 0;
