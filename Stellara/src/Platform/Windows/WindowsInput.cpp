60b23789a50762cadd1e9fb55267bbfe7f4cdc2f
Juanjo <juanjose.acebedolara.23@campusfp.es>
2025-06-23 13:55:53 +0200

    code refactor

--- Stellara/src/Platform/Windows/WindowsInput.cpp
+++ Stellara/src/Platform/Windows/WindowsInput.cpp
@@ -8,22 +8,22 @@ namespace Stellara {
 
 	Input* Input::s_Instance = new WindowsInput();
 
-    bool WindowsInput::IsKeyPressedImpl(int keycode) 
-    {
+    bool WindowsInput::IsKeyPressedImpl(int keycode){
+        
         auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
         auto state = glfwGetKey(window, keycode);
         return state == GLFW_PRESS || state == GLFW_REPEAT;
     }
 
-    bool WindowsInput::IsMouseButtonPressedImpl(int button)
-    {
+    bool WindowsInput::IsMouseButtonPressedImpl(int button){
+
         auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
         auto state = glfwGetMouseButton(window, button);
         return state == GLFW_PRESS;
     }
 
-    std::pair<float, float> WindowsInput::GetMousePositionImpl()
-    {
+    std::pair<float, float> WindowsInput::GetMousePositionImpl(){
+        
         auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
         double xpos, ypos;
         glfwGetCursorPos(window, &xpos, &ypos);
@@ -31,14 +31,14 @@ namespace Stellara {
         return { (float)xpos, (float)ypos };
     }
 
-    float WindowsInput::GetMouseXImpl()
-    {
+    float WindowsInput::GetMouseXImpl(){
+
         auto[x, y] = GetMousePositionImpl();
 		return x;
     }
 
-    float WindowsInput::GetMouseYImpl()
-    {
+    float WindowsInput::GetMouseYImpl(){
+
         auto [x, y] = GetMousePositionImpl();
         return y;
     }
