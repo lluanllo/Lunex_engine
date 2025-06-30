8587e90ea0dea6bb388763a49b41e51695fd0591
juanjo <jjacebedol@gmail.com>
2025-06-30 18:19:44 +0200

    creating the base of a shader class

--- Stellara/src/Renderer/Shader.cpp
+++ Stellara/src/Renderer/Shader.cpp
@@ -4,5 +4,19 @@
 
 namespace Stellara {
 
+	Shader::Shader(const std::string& vertexSource, const std::string& fragmentSource) {
 
+	}
+
+	Shader::~Shader() {
+
+	}
+
+	void Shader::Bind() const {
+
+	}
+
+	void Shader::Unbind() const {
+
+	}
 }
