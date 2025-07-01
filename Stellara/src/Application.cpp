966b532b6dde3446bd6ea491dac3a38becb0fd6e
Juanjo <juanjose.acebedolara.23@campusfp.es>
2025-07-01 18:58:44 +0200

    error solved sahder renderer working

--- Stellara/src/Application.cpp
+++ Stellara/src/Application.cpp
@@ -47,11 +47,11 @@ namespace Stellara{
 
 		std::string vertexSrc = R"(
 			#version 330 core
-
+			
 			layout(location = 0) in vec3 a_Position;
-
+			
 			out vec3 v_Position;
-
+			
 			void main()
 			{
 				v_Position = a_Position;
@@ -61,11 +61,11 @@ namespace Stellara{
 
 		std::string fragmentSrc = R"(
 			#version 330 core
-
+			
 			layout(location = 0) out vec4 color;
-
+			
 			in vec3 v_Position;
-
+			
 			void main()
 			{
 				color = vec4(v_Position * 0.5 + 0.5, 1.0);
