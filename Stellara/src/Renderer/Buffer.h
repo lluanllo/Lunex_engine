30735fdb5ffadcf51fd26b4aecfe685a10129aa2
juanjo <jjacebedol@gmail.com>
2025-07-04 18:41:31 +0200

    set up of the layouts

--- Stellara/src/Renderer/Buffer.h
+++ Stellara/src/Renderer/Buffer.h
@@ -1,6 +1,33 @@
 #pragma once
 
 namespace Stellara {
+
+	enum class ShaderDataType {
+		None = 0,
+		Float, Float2, Float3, Float4,
+		Int, Int2, Int3, Int4,
+		Mat3, Mat4,
+		Bool
+	};
+
+	struct BufferElements {
+		std::string Name;
+		ShaderDataType Type;
+		uint32_t Size;
+		uint32_t Offset;
+
+		BufferElements(const std::string& name, ShaderDataType type)
+			: Name(name), Type(type), Size(0), Offset(0) {
+
+		}
+	};
+
+	class STELLARA_API BufferLayout {
+		public:
+				inline const std::vector<BufferElements>& GetElements() const { return m_Elements; }
+		private:
+			std::vector<BufferElements> m_Elements;
+	};
 	
 	class STELLARA_API VertexBuffer {
 		public:
