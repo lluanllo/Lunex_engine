fddc193e25eb5969accff11e9fcc2e931289f3e8
juanjo <jjacebedol@gmail.com>
2025-07-05 20:18:45 +0200

    vertex layout finish

--- Stellara/src/Renderer/Buffer.h
+++ Stellara/src/Renderer/Buffer.h
@@ -34,10 +34,34 @@ namespace Stellara {
 		ShaderDataType Type;
 		uint32_t Size;
 		uint32_t Offset;
+		bool Normalized;
 
-		BufferElement(ShaderDataType type, const std::string& name)
-			: Name(name), Type(type), Size(ShaderDataTypeSize(type)), Offset(0) {
+		BufferElement();
 
+		BufferElement(ShaderDataType type, const std::string& name, bool normalized = false)
+			: Name(name), Type(type), Size(ShaderDataTypeSize(type)), Offset(0), Normalized(normalized)
+		{
+		}
+
+		uint32_t GetComponentCount() const
+		{
+			switch (Type)
+			{
+			case ShaderDataType::Float:   return 1;
+			case ShaderDataType::Float2:  return 2;
+			case ShaderDataType::Float3:  return 3;
+			case ShaderDataType::Float4:  return 4;
+			case ShaderDataType::Mat3:    return 3 * 3;
+			case ShaderDataType::Mat4:    return 4 * 4;
+			case ShaderDataType::Int:     return 1;
+			case ShaderDataType::Int2:    return 2;
+			case ShaderDataType::Int3:    return 3;
+			case ShaderDataType::Int4:    return 4;
+			case ShaderDataType::Bool:    return 1;
+			}
+
+			ST_CORE_ASSERT(false, "Unknown ShaderDataType!");
+			return 0;
 		}
 	};
 
@@ -47,7 +71,14 @@ namespace Stellara {
 				: m_Elements(element) {
 				CalculateOffsetAndStride();
 			}
+
+			inline uint32_t GetStride() const { return m_Stride; }
 			inline const std::vector<BufferElement>& GetElements() const { return m_Elements; }
+
+			std::vector<BufferElement>::iterator begin() { return m_Elements.begin(); }
+			std::vector<BufferElement>::iterator end() { return m_Elements.end(); }
+			std::vector<BufferElement>::const_iterator begin() const { return m_Elements.begin(); }
+			std::vector<BufferElement>::const_iterator end() const { return m_Elements.end(); }
 	
 		private:
 			void CalculateOffsetAndStride(){
@@ -76,7 +107,7 @@ namespace Stellara {
 			virtual void Bind() const = 0;
 			virtual void Unbind() const = 0;
 			
-			virtual void GetLayout() const = 0;
+			virtual const BufferLayout& GetLayout() const = 0;
 			virtual void SetLayout(const BufferLayout& layout) = 0;
 			
 			static VertexBuffer* Create(float* vertices, uint32_t size);
