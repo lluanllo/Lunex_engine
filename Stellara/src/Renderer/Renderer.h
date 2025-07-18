851f45c9e70fa0ebdd310c513132e2b70676e999
Juanjo <juanjose.acebedolara.23@campusfp.es>
2025-07-18 18:37:58 +0200

    creating the base for a render command

--- Stellara/src/Renderer/Renderer.h
+++ Stellara/src/Renderer/Renderer.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "RenderCommand.h"
+
 namespace Stellara {
 	
 	enum class STELLARA_API RendererAPI {
---
+++ Stellara/src/Renderer/RendererAPI.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Core/Core.h"
+#include "VertexArray.h"
+
+#include "glm/glm.hpp"
+
+namespace Stellara {
+
+	class STELLARA_API RendererAPI {
+		public:
+			enum class API {
+				None = 0, OpenGL = 1
+			};
+	         
+			static API GetAPI() { return s_API; }
+			
+		private:
+			virtual void SetClearColor(const glm::vec4& color) = 0;
+			virtual void Clear() = 0;
+
+			virtual void DrawIndexed(const std::shared_ptr<class VertexArray>& vertexArray, uint32_t indexCount = 0) = 0;
+	    private:
+			static API s_API;
+	};
+}
