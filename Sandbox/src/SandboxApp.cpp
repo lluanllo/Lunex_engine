0af4a25c7b1cbb0ccf24f2fc3849cc86d3a40d83
Juanjo <juanjose.acebedolara.23@campusfp.es>
2025-09-02 21:36:50 +0200

    cleaning SandboxApp

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -10,24 +10,16 @@ class ExampleLayer : public Stellara::Layer{
 
 		void OnUpdate() override {
 			
-			if (Stellara::Input::IsKeyPressed(ST_KEY_TAB)) {
-				STLR_LOG_INFO("Tab key is pressed!");
-			}
 		}
 
 		
 		virtual void OnImGuiRender() override {
-			ImGui::Begin("Test");
-			ImGui::Text("Hello World");
-			ImGui::End();
+			
 		}
 		
 
 		void OnEvent(Stellara::Event& event) override {
-			if (event.GetEventType() == Stellara::EventType::KeyPressed) {
-				Stellara::KeyPressedEvent& e = (Stellara::KeyPressedEvent&)event;
-				STLR_LOG_TRACE("Key pressed: {0}", (char)e.GetKeyCode());
-			}
+			
 		}
 
 };
