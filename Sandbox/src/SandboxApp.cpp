31ef50a4af60dd90d26e4a812ac0c25005e2ca71
Juanjo <juanjose.acebedolara.23@campusfp.es>
2025-09-08 21:17:33 +0200

    tranform finish

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -6,7 +6,7 @@
 class ExampleLayer : public Lunex::Layer{
 	public:
 		ExampleLayer()
-			: Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f), m_SquarePosition(0.0f) {
+			: Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f) {
 			
 			m_VertexArray.reset(Lunex::VertexArray::Create());
 			
@@ -33,26 +33,26 @@ class ExampleLayer : public Lunex::Layer{
 			m_SquareVA.reset(Lunex::VertexArray::Create());
 			
 			float squareVertices[3 * 4] = {
-				-0.75f, -0.75f, 0.0f,
-				 0.75f, -0.75f, 0.0f,
-				 0.75f,  0.75f, 0.0f,
-				-0.75f,  0.75f, 0.0f
+				-0.5f, -0.5f, 0.0f,
+				 0.5f, -0.5f, 0.0f,
+				 0.5f,  0.5f, 0.0f,
+				-0.5f,  0.5f, 0.0f
 			};
 			
 			std::shared_ptr<Lunex::VertexBuffer> squareVB;
 			squareVB.reset(Lunex::VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
-
+			
 			squareVB->SetLayout({
 				{ Lunex::ShaderDataType::Float3, "a_Position" }
 				});
-
+			
 			m_SquareVA->AddVertexBuffer(squareVB);
-
+			
 			uint32_t squareIndices[6] = { 0, 1, 2, 2, 3, 0 };
 			std::shared_ptr<Lunex::IndexBuffer> squareIB;
 			squareIB.reset(Lunex::IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t)));
 			m_SquareVA->SetIndexBuffer(squareIB);
-
+				
 			std::string vertexSrc = R"(
 			#version 330 core
 			
@@ -72,7 +72,7 @@ class ExampleLayer : public Lunex::Layer{
 				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
 			}
 		)";
-
+			
 			std::string fragmentSrc = R"(
 			#version 330 core
 			
@@ -87,9 +87,9 @@ class ExampleLayer : public Lunex::Layer{
 				color = v_Color;
 			}
 		)";
-
+			
 			m_Shader.reset(new Lunex::Shader(vertexSrc, fragmentSrc));
-
+			
 			std::string blueShaderVertexSrc = R"(
 			#version 330 core
 			
@@ -106,7 +106,7 @@ class ExampleLayer : public Lunex::Layer{
 				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
 			}
 		)";
-
+			
 			std::string blueShaderFragmentSrc = R"(
 			#version 330 core
 			
@@ -122,7 +122,7 @@ class ExampleLayer : public Lunex::Layer{
 			
 			m_BlueShader.reset(new Lunex::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
 		}
-
+		
 		void OnUpdate(Lunex::Timestep ts) override {
 			
 			if (Lunex::Input::IsKeyPressed(LN_KEY_LEFT))
@@ -140,42 +140,39 @@ class ExampleLayer : public Lunex::Layer{
 			else if (Lunex::Input::IsKeyPressed(LN_KEY_D))
 				m_CameraRotation += m_CameraRotationSpeed * ts;
 			
-			if (Lunex::Input::IsKeyPressed(LN_KEY_J))
-				m_SquarePosition.x += m_SquareMoveSpeed * ts;
-			else if (Lunex::Input::IsKeyPressed(LN_KEY_L))
-				m_SquarePosition.x -= m_SquareMoveSpeed;
-			
-			if (Lunex::Input::IsKeyPressed(LN_KEY_I))
-				m_SquarePosition.y -= m_SquareMoveSpeed * ts;
-			else if (Lunex::Input::IsKeyPressed(LN_KEY_K))
-				m_SquarePosition.y += m_SquareMoveSpeed * ts;
-			
 			Lunex::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
 			Lunex::RenderCommand::Clear();
 			
 			m_Camera.SetPosition(m_CameraPosition);
 			m_Camera.SetRotation(m_CameraRotation);
 			
-			glm::mat4 transform = glm::translate(glm::mat4(1.0f), m_SquarePosition);
-			
 			Lunex::Renderer::BeginScene(m_Camera);
 			
-			Lunex::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+			static glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+			
+			for (int y = 0; y < 20; y++) {
+				for (int x = 0; x < 20; x++) {
+					glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+					glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+					Lunex::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+				}
+			}
+			
 			Lunex::Renderer::Submit(m_Shader, m_VertexArray);
 			
 			Lunex::Renderer::EndScene();
 		}
-
+		
 		
 		virtual void OnImGuiRender() override {
 			
 		}
-
+		
 		void OnEvent(Lunex::Event& event) override {
 			Lunex::EventDispatcher dispatcher(event);
 			dispatcher.Dispatch<Lunex::KeyPressedEvent>(LNX_BIND_EVENT_FN(ExampleLayer::OnKeyPressEvent));
 		}
-
+		
 		bool OnKeyPressEvent(Lunex::KeyPressedEvent& event) {
 			return false;
 		}
@@ -193,9 +190,6 @@ class ExampleLayer : public Lunex::Layer{
 			float m_CameraRotation = 0.0f;
 			float m_CameraRotationSpeed = 180.0f;
 			float m_CameraMoveSpeed = 5.0f;
-
-			glm::vec3 m_SquarePosition;
-			float m_SquareMoveSpeed = 2.0f;
 };
 
 class Sandbox : public Lunex::Application{
