05ff1b2a2dac23f9fbb4e8ce904dece08d026444
Juanjo <juanjose.acebedolara.23@campusfp.es>
2025-09-11 01:27:27 +0200

    added stb_image lib

--- Lunex/src/Platform/OpenGL/OpenGLTexture.cpp
+++ Lunex/src/Platform/OpenGL/OpenGLTexture.cpp
@@ -2,6 +2,18 @@
 
 #include "OpenGLTexture.h"
 
-namespace Lunex {
+#include <glad/glad.h>
 
+namespace Lunex {
+	OpenGLTexture2D::OpenGLTexture2D(const std::string& path) : m_Path(path) {
+		
+	}
+	
+	OpenGLTexture2D::~OpenGLTexture2D() {
+		
+	}
+	
+	void OpenGLTexture2D::Bind(uint32_t slot) const {
+		
+	}
 }
--- Lunex/src/Platform/OpenGL/OpenGLTexture.h
+++ Lunex/src/Platform/OpenGL/OpenGLTexture.h
@@ -9,9 +9,14 @@ namespace Lunex {
 			OpenGLTexture2D(const std::string& path);
 			virtual ~OpenGLTexture2D();
 			
-			virtual uint32_t GetWidth() const { return m_Width; }
-			virtual uint32_t GetHeight() const { return m_Height; }
+			virtual uint32_t GetWidth() const override { return m_Width; };
+			virtual uint32_t GetHeight() const override { return m_Height;  };
 			
 			virtual void Bind(uint32_t slot = 0) const;
+			
+		private:
+			std::string m_Path;
+			uint32_t m_Width, m_Height;
+			uint32_t m_RendererID;
 	};
 }
