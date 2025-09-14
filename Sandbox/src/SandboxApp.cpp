2d20abdc1466aa9fa27419f50296c6e47be13d44
Juanjo <juanjose.acebedolara.23@campusfp.es>
2025-09-14 23:44:48 +0200

    adding gl blending to the engine

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -128,41 +128,41 @@ class ExampleLayer : public Lunex::Layer{
 			m_FlatColorShader.reset(Lunex::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 			
 			std::string textureShaderVertexSrc = R"(
-			#version 330 core
+				#version 330 core
+				
+				layout(location = 0) in vec3 a_Position;
+				layout(location = 1) in vec2 a_TexCoord;
+				
+				uniform mat4 u_ViewProjection;
+				uniform mat4 u_Transform;
+				
+				out vec2 v_TexCoord;
+				
+				void main() {
+					v_TexCoord = a_TexCoord;
+					gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
+				}
+			)";
 			
-			layout(location = 0) in vec3 a_Position;
-			layout(location = 1) in vec2 a_TexCoord;
-
-			uniform mat4 u_ViewProjection;
-			uniform mat4 u_Transform;
-
-			out vec2 v_TexCoord;
-
-			void main()
-			{
-				v_TexCoord = a_TexCoord;
-				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
-			}
-		)";
-
 			std::string textureShaderFragmentSrc = R"(
-			#version 330 core
+				#version 330 core
+				
+				layout(location = 0) out vec4 color;
+				
+				in vec2 v_TexCoord;
+				
+				uniform sampler2D u_Texture;
+				
+				void main() {
+					color = texture(u_Texture, v_TexCoord);
+				}
+			)";
 			
-			layout(location = 0) out vec4 color;
-
-			in vec2 v_TexCoord;
-			
-			uniform sampler2D u_Texture;
-
-			void main()
-			{
-				color = texture(u_Texture, v_TexCoord);
-			}
-		)";
-
 			m_TextureShader.reset(Lunex::Shader::Create(textureShaderVertexSrc, textureShaderFragmentSrc));
 			
 			m_Texture = Lunex::Texture2D::Create("assets/textures/Checkerboard.png");
+			m_ChernoLogoTexture = Lunex::Texture2D::Create("assets/textures/ChernoLogo.png");
+			
 			std::dynamic_pointer_cast<Lunex::OpenGLShader>(m_TextureShader)->Bind();
 			std::dynamic_pointer_cast<Lunex::OpenGLShader>(m_TextureShader)->UploadUniformInt("u_Texture", 0);
 		}
@@ -208,6 +208,9 @@ class ExampleLayer : public Lunex::Layer{
 			m_Texture->Bind();
 			Lunex::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
 			
+			m_ChernoLogoTexture->Bind();
+			Lunex::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
+			
 			//Lunex::Renderer::Submit(m_Shader, m_VertexArray);
 			
 			Lunex::Renderer::EndScene();
@@ -236,7 +239,7 @@ class ExampleLayer : public Lunex::Layer{
 			Lunex::Ref<Lunex::Shader> m_FlatColorShader, m_TextureShader;
 			Lunex::Ref<Lunex::VertexArray> m_SquareVA;
 			
-			Lunex::Ref<Lunex::Texture2D> m_Texture;
+			Lunex::Ref<Lunex::Texture2D> m_Texture, m_ChernoLogoTexture;
 			
 			Lunex::OrthographicCamera m_Camera;
 			glm::vec3 m_CameraPosition;
