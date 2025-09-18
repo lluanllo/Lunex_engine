0d3e07c0dff3727e9336f2d818d0392a3d2506d9
lluanllo <jjacebedol@gmail.com>
2025-09-18 14:11:50 +0200

    shader libririe logic finish

--- Lunex/src/Renderer/Shader.cpp
+++ Lunex/src/Renderer/Shader.cpp
@@ -8,20 +8,20 @@
 
 namespace Lunex {
 	
-	Shader* Shader::Create(const std::string& filepath) {
+	Ref<Shader> Shader::Create(const std::string& filepath) {
 		switch (Renderer::GetAPI()) {
 			case RendererAPI::API::None:    LN_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
-			case RendererAPI::API::OpenGL:  return new OpenGLShader(filepath);
+			case RendererAPI::API::OpenGL:  std::make_shared<OpenGLShader>(filepath);
 		}
 		
 		LN_CORE_ASSERT(false, "Unknown RendererAPI!");
 		return nullptr;
 	}
 	
-	Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc) {
+	Ref<Shader> Shader::Create(const std::string& name, const std::string& vertexSrc, const std::string& fragmentSrc) {
 		switch (Renderer::GetAPI()) {
 			case RendererAPI::API::None:    LN_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
-			case RendererAPI::API::OpenGL:  return new OpenGLShader(vertexSrc, fragmentSrc);
+			case RendererAPI::API::OpenGL:  std::make_shared<OpenGLShader>(name, vertexSrc, fragmentSrc);
 		}
 		
 		LN_CORE_ASSERT(false, "Unknown RendererAPI!");
@@ -29,18 +29,33 @@ namespace Lunex {
 	}
 	
 	void SahderLibrary::Add(const Ref<Shader>& shader) {
-		
+		auto& name = shader->GetName();
+		Add(name, shader);
+	}
+
+	void SahderLibrary::Add(const std::string& name, const Ref<Shader>& shader) {
+		LN_CORE_ASSERT(!Exists(name), "Shader already exists!");
+		m_Shaders[name] = shader;
 	}
 	
 	Ref<Shader> SahderLibrary::Load(const std::string& filepath) {
-		return Ref<Shader>();
+		auto shader = Ref<Shader>(Shader::Create(filepath));
+		Add(shader);
+		return shader;
 	}
 	
 	Ref<Shader> SahderLibrary::Load(const std::string& name, const std::string& filepath) {
-		return Ref<Shader>();
+		auto shader = Shader::Create(filepath);
+		Add(name, shader);
+		return shader;
 	}
 	
 	Ref<Shader> SahderLibrary::Get(const std::string& name) {
-		return Ref<Shader>();
+		LN_CORE_ASSERT(m_Shaders.find(name) != m_Shaders.end(), "Shader not found!");
+		return m_Shaders[name];
+	}
+
+	bool SahderLibrary::Exists(const std::string& name) const {
+		return m_Shaders.find(name) != m_Shaders.end();
 	}
 }
