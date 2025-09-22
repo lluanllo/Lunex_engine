b3a6b32dbcd9033a42f1a08e23c7849f86b27f5c
Juanjo <juanjose.acebedolara.23@campusfp.es>
2025-09-22 23:59:57 +0200

    import issue fixed

--- Lunex/src/ImGui/ImGuiLayer.cpp
+++ Lunex/src/ImGui/ImGuiLayer.cpp
@@ -6,7 +6,7 @@
 #include "backends/imgui_impl_opengl3.h"
 #include "backends/imgui_impl_opengl3_loader.h"
 
-#include "Application.h"
+#include "Core/Application.h"
 
 // TEMPORARY
 #include <GLFW/glfw3.h>
