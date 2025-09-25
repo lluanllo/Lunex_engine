a14707a4add71d8a99f0d4372ca754656e98162b
Juanjo <juanjose.acebedolara.23@campusfp.es>
2025-09-25 23:56:00 +0200

    added 2D transforms

--- Lunex/src/Renderer/Renderer2D.cpp
+++ Lunex/src/Renderer/Renderer2D.cpp
@@ -1,6 +1,8 @@
 #include "stpch.h"
 #include "Renderer2D.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 #include "VertexArray.h"
 #include "Shader.h"
 #include "RenderCommand.h"
@@ -60,6 +62,8 @@ namespace Lunex {
 	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color) {
 		s_Data->FlatColorShader->Bind();
 		s_Data->FlatColorShader->SetFloat4("u_Color", color);
+		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
+		s_Data->FlatColorShader->SetMat4("u_Transform", transform);
 		
 		s_Data->QuadVertexArray->Bind();
 		RenderCommand::DrawIndexed(s_Data->QuadVertexArray);
--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -18,8 +18,7 @@ void Sandbox2D::OnAttach() {
 void Sandbox2D::OnDetach() {
 }
 
-void Sandbox2D::OnUpdate(Lunex::Timestep ts)
-{
+void Sandbox2D::OnUpdate(Lunex::Timestep ts) {
 	// Update
 	m_CameraController.OnUpdate(ts);
 	
@@ -28,22 +27,17 @@ void Sandbox2D::OnUpdate(Lunex::Timestep ts)
 	Lunex::RenderCommand::Clear();
 	
 	Lunex::Renderer2D::BeginScene(m_CameraController.GetCamera());
-	Lunex::Renderer2D::DrawQuad({ 0.0f, 0.0f }, { 1.0f, 1.0f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+	Lunex::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+	Lunex::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
 	Lunex::Renderer2D::EndScene();
-	
-	// TODO: Add these functions - Shader::SetMat4, Shader::SetFloat4
-	// std::dynamic_pointer_cast<Lunex::OpenGLShader>(m_FlatColorShader)->Bind();
-	// std::dynamic_pointer_cast<Lunex::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat4("u_Color", m_SquareColor);
 }
 
-void Sandbox2D::OnImGuiRender()
-{
+void Sandbox2D::OnImGuiRender() {
 	ImGui::Begin("Settings");
 	ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
 	ImGui::End();
 }
 
-void Sandbox2D::OnEvent(Lunex::Event& e)
-{
+void Sandbox2D::OnEvent(Lunex::Event& e) {
 	m_CameraController.OnEvent(e);
 }
