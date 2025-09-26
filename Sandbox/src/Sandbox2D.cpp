16759b1ed21fd6fc9caa813928f972a994306bba
Juanjo <juanjose.acebedolara.23@campusfp.es>
2025-09-26 21:13:31 +0200

    added 2D textures suport for the 2d renderer

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -12,7 +12,8 @@ Sandbox2D::Sandbox2D()
 }
 
 void Sandbox2D::OnAttach() {
-	
+	m_CheckerboardTexture = Lunex::Texture2D::Create("assets/textures/Checkerboard.png");
+
 }
 
 void Sandbox2D::OnDetach() {
@@ -29,6 +30,7 @@ void Sandbox2D::OnUpdate(Lunex::Timestep ts) {
 	Lunex::Renderer2D::BeginScene(m_CameraController.GetCamera());
 	Lunex::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 	Lunex::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
+	Lunex::Renderer2D::DrawQuad({ 0.2f, 0.5f}, {0.5f, 0.5f }, m_CheckerboardTexture);
 	Lunex::Renderer2D::EndScene();
 }
 
