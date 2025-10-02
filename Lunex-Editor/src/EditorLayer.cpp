5b418f8744c158a5b1132b518dd827d587ec58a1
Juanjo <juanjose.acebedolara.23@campusfp.es>
2025-10-02 18:57:55 +0200

    viewport resizing added and style for no margins

--- Lunex-Editor/src/EditorLayer.cpp
+++ Lunex-Editor/src/EditorLayer.cpp
@@ -134,13 +134,21 @@ namespace Lunex {
 			
 			ImGui::End();
 			
+			ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2{ 0, 0 });
 			ImGui::Begin("Viewport");
 			ImVec2 viewportPanelSize = ImGui::GetContentRegionAvail();
+			if (m_ViewportSize != *((glm::vec2*)&viewportPanelSize)) {
+				m_Framebuffer->Resize((uint32_t)viewportPanelSize.x, (uint32_t)viewportPanelSize.y);
+				m_ViewportSize = { viewportPanelSize.x, viewportPanelSize.y };
+				
+				m_CameraController.OnResize(viewportPanelSize.x, viewportPanelSize.y);
+			}
 			LNX_LOG_WARN("ViewPort Size: {0}, {1}", viewportPanelSize.x, viewportPanelSize.y);
 			uint32_t textureID = m_Framebuffer->GetColorAttachmentRendererID();
-			ImGui::Image((void*)textureID, ImVec2{ 1280, 720 });
-			
+			ImGui::Image((void*)textureID, ImVec2{ m_ViewportSize.x, m_ViewportSize.y }, ImVec2{ 0, 1 }, ImVec2{ 1, 0 });
 			ImGui::End();
+			ImGui::PopStyleVar();
+			
 			ImGui::End();
 		}
 		
