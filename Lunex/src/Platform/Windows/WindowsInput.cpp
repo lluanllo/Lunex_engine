bab23db3567e4f2aabf05d53c758bc67fa5d9bef
Juanjo <juanjose.acebedolara.23@campusfp.es>
2025-10-03 17:38:15 +0200

    fixing key and mouse codes

--- Lunex/src/Platform/Windows/WindowsInput.cpp
+++ Lunex/src/Platform/Windows/WindowsInput.cpp
@@ -1,45 +1,38 @@
 #include "stpch.h"
-#include "WindowsInput.h"
+#include "Core/Input.h"
 
-#include <GLFW/glfw3.h>
 #include "Core/Application.h"
+#include <GLFW/glfw3.h>
 
 namespace Lunex {
-
-    Scope<Input> Input::s_Instance = CreateScope<WindowsInput>();
-
-    bool WindowsInput::IsKeyPressedImpl(int keycode){
-        
-        auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
-        auto state = glfwGetKey(window, keycode);
-        return state == GLFW_PRESS || state == GLFW_REPEAT;
-    }
-
-    bool WindowsInput::IsMouseButtonPressedImpl(int button){
-
-        auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
-        auto state = glfwGetMouseButton(window, button);
-        return state == GLFW_PRESS;
-    }
-
-    std::pair<float, float> WindowsInput::GetMousePositionImpl(){
-        
-        auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
-        double xpos, ypos;
-        glfwGetCursorPos(window, &xpos, &ypos);
-
-        return { (float)xpos, (float)ypos };
-    }
-
-    float WindowsInput::GetMouseXImpl(){
-
-        auto[x, y] = GetMousePositionImpl();
+	bool Input::IsKeyPressed(KeyCode key) {
+		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		auto state = glfwGetKey(window, static_cast<int32_t>(key));
+		return state == GLFW_PRESS || state == GLFW_REPEAT;
+	}
+	
+	bool Input::IsMouseButtonPressed(MouseCode button) {
+		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		auto state = glfwGetMouseButton(window, static_cast<int32_t>(button));
+		return state == GLFW_PRESS;
+	}
+	
+	std::pair<float, float> Input::GetMousePosition() {
+		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		double xpos, ypos;
+		glfwGetCursorPos(window, &xpos, &ypos);
+		
+		return { (float)xpos, (float)ypos };
+	}
+	
+	float Input::GetMouseX() {
+		auto [x, y] = GetMousePosition();
 		return x;
-    }
-
-    float WindowsInput::GetMouseYImpl(){
-
-        auto [x, y] = GetMousePositionImpl();
-        return y;
-    }
+	}
+	
+	float Input::GetMouseY()
+	{
+		auto [x, y] = GetMousePosition();
+		return y;
+	}
 }
--- Lunex/src/Renderer/OrthographicCameraController.cpp
+++ Lunex/src/Renderer/OrthographicCameraController.cpp
@@ -3,7 +3,7 @@
 #include "OrthographicCameraController.h"
 
 #include "Core/Input.h"
-#include "Codes/KeyCodes.h"
+#include "Core/KeyCodes.h"
 
 namespace Lunex {
 	OrthographicCameraController::OrthographicCameraController(float aspectRatio, bool rotation)
@@ -14,33 +14,28 @@ namespace Lunex {
 	
 	void OrthographicCameraController::OnUpdate(Timestep ts) {
 		LNX_PROFILE_FUNCTION();
-		if (Input::IsKeyPressed(LN_KEY_A))
-		{
+		if (Input::IsKeyPressed(Key::A)) {
 			m_CameraPosition.x -= cos(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
 			m_CameraPosition.y -= sin(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
 		}
-		else if (Input::IsKeyPressed(LN_KEY_D))
-		{
+		else if (Input::IsKeyPressed(Key::D)) {
 			m_CameraPosition.x += cos(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
 			m_CameraPosition.y += sin(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
 		}
 		
-		if (Input::IsKeyPressed(LN_KEY_W))
-		{
+		if (Input::IsKeyPressed(Key::W)) {
 			m_CameraPosition.x += -sin(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
 			m_CameraPosition.y += cos(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
 		}
-		else if (Input::IsKeyPressed(LN_KEY_S))
-		{
+		else if (Input::IsKeyPressed(Key::S)) {
 			m_CameraPosition.x -= -sin(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
 			m_CameraPosition.y -= cos(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
 		}
 		
-		if (m_Rotation)
-		{
-			if (Input::IsKeyPressed(LN_KEY_Q))
+		if (m_Rotation) {
+			if (Input::IsKeyPressed(Key::Q))
 				m_CameraRotation += m_CameraRotationSpeed * ts;
-			if (Input::IsKeyPressed(LN_KEY_E))
+			if (Input::IsKeyPressed(Key::E))
 				m_CameraRotation -= m_CameraRotationSpeed * ts;
 			
 			if (m_CameraRotation > 180.0f)
