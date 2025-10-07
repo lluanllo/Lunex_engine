0ff72f3b7f1fccc3ab182b4aeaf1b02c7998a73b
Juanjo <juanjose.acebedolara.23@campusfp.es>
2025-10-08 00:05:42 +0200

    modifiying bind macro

--- Lunex/src/Core/Core.h
+++ Lunex/src/Core/Core.h
@@ -82,7 +82,7 @@
 
 #define BIT(x) (1 << x)
 
-#define LN_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
+#define LNX_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
 
 namespace Lunex {
      
