ec33df86fdcb498fc543c8627046d25ad575bbd9
Juanjo <juanjose.acebedolara.23@campusfp.es>
2025-10-10 21:20:41 +0200

    added removing and adding entities and the posibility to add diferent components

--- Lunex-Editor/src/Panels/SceneHierarchyPanel.cpp
+++ Lunex-Editor/src/Panels/SceneHierarchyPanel.cpp
@@ -28,11 +28,37 @@ namespace Lunex {
 		if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered())
 			m_SelectionContext = {};
 		
+		// Right-click on blank space
+		if (ImGui::BeginPopupContextWindow(nullptr, ImGuiPopupFlags_MouseButtonRight | ImGuiPopupFlags_NoOpenOverItems)) {
+			if (ImGui::MenuItem("Create Empty Entity"))
+				m_Context->CreateEntity("Empty Entity");
+			
+			ImGui::EndPopup();
+		}
+		
 		ImGui::End();
 		
 		ImGui::Begin("Properties");
-		if (m_SelectionContext)
+		if (m_SelectionContext) {
 			DrawComponents(m_SelectionContext);
+			
+			if (ImGui::Button("Add Component"))
+				ImGui::OpenPopup("AddComponent");
+			
+			if (ImGui::BeginPopup("AddComponent")) {
+				if (ImGui::MenuItem("Camera")) {
+					m_SelectionContext.AddComponent<CameraComponent>();
+					ImGui::CloseCurrentPopup();
+				}
+				
+				if (ImGui::MenuItem("Sprite Renderer")) {
+					m_SelectionContext.AddComponent<SpriteRendererComponent>();
+					ImGui::CloseCurrentPopup();
+				}
+				
+				ImGui::EndPopup();
+			}	
+		}
 		
 		ImGui::End();
 	}
@@ -46,12 +72,26 @@ namespace Lunex {
 			m_SelectionContext = entity;
 		}
 		
+		bool entityDeleted = false;
+		if (ImGui::BeginPopupContextItem()) {
+			if (ImGui::MenuItem("Delete Entity"))
+				entityDeleted = true;
+			
+			ImGui::EndPopup();
+		}
+		
 		if (opened) {
 			ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags_OpenOnArrow;
 			bool opened = ImGui::TreeNodeEx((void*)9817239, flags, tag.c_str());
 			if (opened)
 				ImGui::TreePop();
 			ImGui::TreePop();
+		}
+		
+		if (entityDeleted) {
+			m_Context->DestroyEntity(entity);
+			if (m_SelectionContext == entity)
+				m_SelectionContext = {};
 		}
 	}
 	
@@ -123,8 +163,12 @@ namespace Lunex {
 			}
 		}
 		
+		const ImGuiTreeNodeFlags treeNodeFlags = ImGuiTreeNodeFlags_DefaultOpen | ImGuiTreeNodeFlags_AllowItemOverlap;
+		
 		if (entity.HasComponent<TransformComponent>()) {
-			if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform")) {
+			bool open = ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), treeNodeFlags, "Transform");
+			
+			if (open) {
 				auto& tc = entity.GetComponent<TransformComponent>();
 				DrawVec3Control("Translation", tc.Translation);
 				glm::vec3 rotation = glm::degrees(tc.Rotation);
@@ -137,7 +181,7 @@ namespace Lunex {
 		}
 		
 		if (entity.HasComponent<CameraComponent>()) {
-			if (ImGui::TreeNodeEx((void*)typeid(CameraComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Camera")) {
+			if (ImGui::TreeNodeEx((void*)typeid(CameraComponent).hash_code(), treeNodeFlags, "Camera")) {
 				auto& cameraComponent = entity.GetComponent<CameraComponent>();
 				auto& camera = cameraComponent.Camera;
 				
@@ -161,17 +205,17 @@ namespace Lunex {
 				}
 				
 				if (camera.GetProjectionType() == SceneCamera::ProjectionType::Perspective) {
-					float verticalFov = glm::degrees(camera.GetPerspectiveVerticalFOV());
-					if (ImGui::DragFloat("Vertical FOV", &verticalFov))
-						camera.SetPerspectiveVerticalFOV(glm::radians(verticalFov));
+					float perspectiveVerticalFov = glm::degrees(camera.GetPerspectiveVerticalFOV());
+					if (ImGui::DragFloat("Vertical FOV", &perspectiveVerticalFov))
+						camera.SetPerspectiveVerticalFOV(glm::radians(perspectiveVerticalFov));
 					
-					float orthoNear = camera.GetPerspectiveNearClip();
-					if (ImGui::DragFloat("Near", &orthoNear))
-						camera.SetPerspectiveNearClip(orthoNear);
+					float perspectiveNear = camera.GetPerspectiveNearClip();
+					if (ImGui::DragFloat("Near", &perspectiveNear))
+						camera.SetPerspectiveNearClip(perspectiveNear);
 					
-					float orthoFar = camera.GetPerspectiveFarClip();
-					if (ImGui::DragFloat("Far", &orthoFar))
-						camera.SetPerspectiveFarClip(orthoFar);
+					float perspectiveFar = camera.GetPerspectiveFarClip();
+					if (ImGui::DragFloat("Far", &perspectiveFar))
+						camera.SetPerspectiveFarClip(perspectiveFar);
 				}
 				
 				if (camera.GetProjectionType() == SceneCamera::ProjectionType::Orthographic) {
@@ -189,16 +233,36 @@ namespace Lunex {
 					
 					ImGui::Checkbox("Fixed Aspect Ratio", &cameraComponent.FixedAspectRatio);
 				}
+				
 				ImGui::TreePop();
 			}
 		}
 		
 		if (entity.HasComponent<SpriteRendererComponent>()) {
-			if (ImGui::TreeNodeEx((void*)typeid(SpriteRendererComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Sprite Renderer")) {
+			ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2{ 4, 4 });
+			bool open = ImGui::TreeNodeEx((void*)typeid(SpriteRendererComponent).hash_code(), treeNodeFlags, "Sprite Renderer");
+			ImGui::SameLine(ImGui::GetWindowWidth() - 25.0f);
+			if (ImGui::Button("+", ImVec2{ 20, 20 })) {
+				ImGui::OpenPopup("ComponentSettings");
+			}
+			ImGui::PopStyleVar();
+			
+			bool removeComponent = false;
+			if (ImGui::BeginPopup("ComponentSettings")) {
+				if (ImGui::MenuItem("Remove component"))
+					removeComponent = true;
+				
+				ImGui::EndPopup();
+			}
+			
+			if (open) {
 				auto& src = entity.GetComponent<SpriteRendererComponent>();
 				ImGui::ColorEdit4("Color", glm::value_ptr(src.Color));
 				ImGui::TreePop();
 			}
+			
+			if (removeComponent)
+				entity.RemoveComponent<SpriteRendererComponent>();
 		}
 	}
 }
--- Lunex/src/Scene/Entity.h
+++ Lunex/src/Scene/Entity.h
@@ -15,7 +15,9 @@ namespace Lunex {
 			template<typename T, typename... Args>
 			T& AddComponent(Args&&... args) {
 				LN_CORE_ASSERT(!HasComponent<T>(), "Entity already has component!");
-				return m_Scene->m_Registry.emplace<T>(m_EntityHandle, std::forward<Args>(args)...);
+				T& component = m_Scene->m_Registry.emplace<T>(m_EntityHandle, std::forward<Args>(args)...);
+				m_Scene->OnComponentAdded<T>(*this, component);
+				return component;
 			}
 			
 			template<typename T>
@@ -36,6 +38,7 @@ namespace Lunex {
 			}
 			
 			operator bool() const { return m_EntityHandle != entt::null; }
+			operator entt::entity() const { return m_EntityHandle; }
 			operator uint32_t() const { return (uint32_t)m_EntityHandle; }
 			
 			bool operator==(const Entity& other) const {
