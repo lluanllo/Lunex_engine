3e34ddabca3304652e4b7ae5d4af6173c84ebda0
Juanjo <juanjose.acebedolara.23@campusfp.es>
2025-10-16 21:30:07 +0200

    added icons to the browser

--- Lunex-Editor/src/Panels/ContentBrowserPanel.h
+++ Lunex-Editor/src/Panels/ContentBrowserPanel.h
@@ -1,19 +1,22 @@
 #pragma once
 
+#include "Renderer/Texture.h"
 #include <filesystem>
 
-#include "Renderer/Texture.h"
-
 namespace Lunex {
-	class ContentBrowserPanel {
-		public:
-			ContentBrowserPanel();
-			
-			void OnImGuiRender();
-		private:
-			std::filesystem::path m_CurrentDirectory;
-			
-			Ref<Texture2D> m_DirectoryIcon;
-			Ref<Texture2D> m_FileIcon;
-	};
+    // Declaración externa para que otros archivos puedan usar g_AssetPath
+    extern const std::filesystem::path g_AssetPath;
+       
+    class ContentBrowserPanel {
+        public:
+            ContentBrowserPanel();
+                
+            void OnImGuiRender();
+                
+        private:
+            std::filesystem::path m_CurrentDirectory;
+             
+            Ref<Texture2D> m_DirectoryIcon;
+            Ref<Texture2D> m_FileIcon;
+    };
 }
