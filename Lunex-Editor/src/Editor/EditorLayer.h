4d7a64d1ff3cb85bf383b89a9eca88c98fed3413
Juanjo <juanjose.acebedolara.23@campusfp.es>
2025-10-17 20:06:53 +0200

    added play button

--- Lunex-Editor/src/Editor/EditorLayer.h
+++ Lunex-Editor/src/Editor/EditorLayer.h
@@ -28,6 +28,12 @@ namespace Lunex {
 			void OpenScene(const std::filesystem::path& path);
 			void SaveSceneAs();
 			
+			void OnScenePlay();
+			void OnSceneStop();
+			
+			// UI Panels
+			void UI_Toolbar();
+			
 		private:
 			Lunex::OrthographicCameraController m_CameraController;
 			
@@ -56,8 +62,16 @@ namespace Lunex {
 			
 			int m_GizmoType = -1;
 			
+			enum class SceneState {
+				Edit = 0, Play = 1
+			};
+			SceneState m_SceneState = SceneState::Edit;
+			
 			// Panels
 			SceneHierarchyPanel m_SceneHierarchyPanel;
 			ContentBrowserPanel m_ContentBrowserPanel;
+			
+			// Editor resources
+			Ref<Texture2D> m_IconPlay, m_IconStop;
 	};
 }
