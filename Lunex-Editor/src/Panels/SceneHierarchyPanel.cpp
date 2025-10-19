86954b78844e5d1e5a31e4f656a48918897b71dd
Juanjo <juanjose.acebedolara.23@campusfp.es>
2025-10-19 23:36:35 +0200

    added circle colliders

--- Lunex-Editor/src/Panels/SceneHierarchyPanel.cpp
+++ Lunex-Editor/src/Panels/SceneHierarchyPanel.cpp
@@ -250,7 +250,6 @@ namespace Lunex {
 		
 		if (ImGui::Button("Add Component"))
 			ImGui::OpenPopup("AddComponent");
-			
 		if (ImGui::BeginPopup("AddComponent")) {
 			if (!m_SelectionContext.HasComponent<CameraComponent>()) {
 				if (ImGui::MenuItem("Camera")) {
@@ -283,6 +282,13 @@ namespace Lunex {
 			if (!m_SelectionContext.HasComponent<CircleRendererComponent>()) {
 				if (ImGui::MenuItem("Circle Renderer"))	{
 					m_SelectionContext.AddComponent<CircleRendererComponent>();
+					ImGui::CloseCurrentPopup();
+				}
+			}
+			
+			if (!m_SelectionContext.HasComponent<CircleCollider2DComponent>()) {
+				if (ImGui::MenuItem("Circle Collider 2D")) {
+					m_SelectionContext.AddComponent<CircleCollider2DComponent>();
 					ImGui::CloseCurrentPopup();
 				}
 			}
@@ -416,6 +422,15 @@ namespace Lunex {
 		DrawComponent<BoxCollider2DComponent>("Box Collider 2D", entity, [](auto& component) {
 			ImGui::DragFloat2("Offset", glm::value_ptr(component.Offset));
 			ImGui::DragFloat2("Size", glm::value_ptr(component.Size));
+			ImGui::DragFloat("Density", &component.Density, 0.01f, 0.0f, 1.0f);
+			ImGui::DragFloat("Friction", &component.Friction, 0.01f, 0.0f, 1.0f);
+			ImGui::DragFloat("Restitution", &component.Restitution, 0.01f, 0.0f, 1.0f);
+			ImGui::DragFloat("Restitution Threshold", &component.RestitutionThreshold, 0.01f, 0.0f);
+		});
+		
+		DrawComponent<CircleCollider2DComponent>("Circle Collider 2D", entity, [](auto& component) {
+			ImGui::DragFloat2("Offset", glm::value_ptr(component.Offset));
+			ImGui::DragFloat("Radius", &component.Radius);
 			ImGui::DragFloat("Density", &component.Density, 0.01f, 0.0f, 1.0f);
 			ImGui::DragFloat("Friction", &component.Friction, 0.01f, 0.0f, 1.0f);
 			ImGui::DragFloat("Restitution", &component.Restitution, 0.01f, 0.0f, 1.0f);
